aa15e36eddcfb7fdd8169959b0f4b703a380deaa
berlin joe d <berlin@capztone.com>
2024-09-09 11:11:11 +0530

    yes my changes

--- honey/settings.py
+++ honey/settings.py
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Already added
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # New line
 
 
 
